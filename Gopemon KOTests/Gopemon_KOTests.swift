ab2d004b5af56913d597074734258a1462351baa
Francesco Thiery <xiomxiom1@gmail.com>
2016-07-26 14:28:26 +0200

    Initial Commit

---
+++ Gopemon KOTests/Gopemon_KOTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gopemon_KOTests.swift
+//  Gopemon KOTests
+//
+//  Created by Francesco Thiery on 26/07/16.
+//  Copyright © 2016 Coocked. All rights reserved.
+//
+
+import XCTest
+@testable import Gopemon_KO
+
+class Gopemon_KOTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
